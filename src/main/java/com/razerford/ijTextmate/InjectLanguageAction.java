9ff0ba3318a3725bb9232898085d44d72bb26b77
RazerFord <desk10567@gmail.com>
2023-07-13 12:46:32 +0300

    feat: added update method in action

--- src/main/java/com/razerford/ijTextmate/InjectLanguageAction.java
+++ src/main/java/com/razerford/ijTextmate/InjectLanguageAction.java
@@ -1,11 +1,25 @@
 package com.razerford.ijTextmate;
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
 public class InjectLanguageAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
+        System.out.println("Hello world");
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        Editor editor = e.getData(CommonDataKeys.EDITOR);
+        Project project = e.getData(CommonDataKeys.PROJECT);
+        e.getPresentation().setEnabledAndVisible(editor != null && project != null);
+    }
+
+    @Override
+    public @NotNull ActionUpdateThread getActionUpdateThread() {
+        return ActionUpdateThread.BGT;
     }
 }
