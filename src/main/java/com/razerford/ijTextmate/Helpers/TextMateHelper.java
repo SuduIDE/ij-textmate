6a87798ad36ac05153207bb4edfa0a77890f10f9
RazerFord <desk10567@gmail.com>
2023-07-15 22:43:33 +0300

    feat: added a class to display all languages ​​supported by textmate

--- src/main/java/com/razerford/ijTextmate/Helpers/TextMateHelper.java
+++ src/main/java/com/razerford/ijTextmate/Helpers/TextMateHelper.java
@@ -1,2 +1,47 @@
-package com.razerford.ijTextmate.Helpers;public class TextMateHelper {
+package com.razerford.ijTextmate.Helpers;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import org.apache.commons.io.FileUtils;
+import org.jetbrains.plugins.textmate.configuration.BundleConfigBean;
+import org.jetbrains.plugins.textmate.configuration.TextMateSettings;
+
+import java.nio.file.Path;
+import java.util.*;
+
+public class TextMateHelper {
+    private final Map<String, String> languages = new HashMap<>();
+    private static final Path FILE_WITH_EXTENSION = Path.of("package.json");
+    private static final String CONTRIBUTES = "contributes";
+    private static final String LANGUAGES = "languages";
+    private static final String EXTENSIONS = "extensions";
+
+    public TextMateHelper() {
+        Collection<BundleConfigBean> bundles = TextMateSettings.getInstance().getBundles();
+        bundles.forEach((bundle) -> languages.put(bundle.getName(), bundle.getPath()));
+    }
+
+    public List<String> getLanguages() {
+        return languages.keySet().stream().toList();
+    }
+
+    public Path getPath(String language) {
+        return Path.of(languages.get(language));
+    }
+
+    public String getExtension(String language) {
+        Path path = getPath(language).resolve(FILE_WITH_EXTENSION);
+        try {
+            String text = FileUtils.readFileToString(path.toFile(), "UTF8");
+            JsonElement root = JsonParser.parseString(text);
+            String extension = root.getAsJsonObject().get(CONTRIBUTES)
+                    .getAsJsonObject().get(LANGUAGES)
+                    .getAsJsonArray().get(0)
+                    .getAsJsonObject().get(EXTENSIONS)
+                    .getAsJsonArray().get(0).toString();
+            if (extension.length() > 3) return extension.substring(2, extension.length() - 1);
+        } catch (Throwable ignore) {
+        }
+        return "";
+    }
 }
