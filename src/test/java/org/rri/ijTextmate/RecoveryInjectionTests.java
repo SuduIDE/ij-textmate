c7452c30d3ecb61075b5664a41f24601122bd52f
RazerFord <desk10567@gmail.com>
2023-07-31 21:09:16 +0300

    fix: fixed test RecoveryInjectionTests

--- src/test/java/org/rri/ijTextmate/RecoveryInjectionTests.java
+++ src/test/java/org/rri/ijTextmate/RecoveryInjectionTests.java
@@ -17,15 +17,11 @@ public class RecoveryInjectionTests extends BasePlatformTestCase {
         return "src/test/testData/PersistentStorageCases";
     }
 
-    public void test0Clear() {
-        PersistentStorage.getInstance(getProject()).getState().clear();
-    }
-
-    public void testWithoutMoving() {
-        checkRecoveryInjection("WithoutMoving.before.java",
-                "WithoutMoving.after.java",
-                "WithoutMoving.java",
-                getMessage("testWithoutMoving"));
+    public void testWithMovingExpressionHard() {
+        checkRecoveryInjection("WithMovingExpressionHard.before.java",
+                "WithMovingExpressionHard.after.java",
+                "WithMovingExpressionHard.java",
+                getMessage("testWithMovingExpressionHard"));
     }
 
     public void testWithMovingExpressionSimple() {
@@ -35,11 +31,12 @@ public class RecoveryInjectionTests extends BasePlatformTestCase {
                 getMessage("testWithMovingExpressionSimple"));
     }
 
-    public void testWithMovingExpressionHard() {
-        checkRecoveryInjection("WithMovingExpressionHard.before.java",
-                "WithMovingExpressionHard.after.java",
-                "WithMovingExpressionHard.java",
-                getMessage("testWithMovingExpressionHard"));
+
+    public void testWithoutMoving() {
+        checkRecoveryInjection("WithoutMoving.before.java",
+                "WithoutMoving.after.java",
+                "WithoutMoving.java",
+                getMessage("testWithoutMoving"));
     }
 
     public void testWithRename() {
@@ -52,17 +49,20 @@ public class RecoveryInjectionTests extends BasePlatformTestCase {
     private void checkRecoveryInjection(String beforeName, String afterName, String baseName, String message) {
         PsiFile[] psiFiles = myFixture.configureByFiles(beforeName, afterName);
         myFixture.renameElement(psiFiles[0], baseName);
-        assertFalse(isInjected(getProject(), myFixture.getEditor(), psiFiles[0]));
+        Project project = myFixture.getProject();
+        Editor editor = myFixture.getEditor();
+        assertFalse(isInjected(project, editor, psiFiles[0]));
 
-        TestHelper.injectLanguage(getProject(), myFixture.getEditor(), psiFiles[0]);
-        assertTrue(isInjected(getProject(), myFixture.getEditor(), psiFiles[0]));
+        TestHelper.injectLanguage(project, editor, psiFiles[0]);
+        assertTrue(isInjected(project, editor, psiFiles[0]));
 
         myFixture.renameElement(psiFiles[0], beforeName);
         myFixture.renameElement(psiFiles[1], baseName);
 
-        new InitializerHighlightListener(getProject()).fileOpened(FileEditorManager.getInstance(getProject()), psiFiles[1].getVirtualFile());
+        PersistentStorage.getInstance(project).getState();
+        new InitializerHighlightListener(project).fileOpened(FileEditorManager.getInstance(project), psiFiles[1].getVirtualFile());
         myFixture.openFileInEditor(psiFiles[1].getVirtualFile());
-        assertTrue(message, isInjected(getProject(), myFixture.getEditor(), psiFiles[1]));
+        assertTrue(message, isInjected(project, editor, psiFiles[1]));
     }
 
     private boolean isInjected(Project project, @NotNull Editor editor, PsiFile psiFile) {
