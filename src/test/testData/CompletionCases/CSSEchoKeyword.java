325e6c5ed8724b857498cee87b30099d77b63d70
RazerFord <desk10567@gmail.com>
2023-08-15 19:16:10 +0300

    fix: added completion tests

---
+++ src/test/testData/CompletionCases/CSSEchoKeyword.java
@@ -0,0 +1,5 @@
+public class Test {
+    public void test() {
+        String shell = "echo<caret> 'Hello, world!'";
+    }
+}
---
+++ src/test/testData/CompletionCases/SQLSELECTKeyword.java
@@ -0,0 +1,5 @@
+public class Test {
+    public void test() {
+        String sql = "SELECT<caret> * FROM users";
+    }
+}
